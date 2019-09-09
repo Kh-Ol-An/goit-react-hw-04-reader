84f307508f1206f018ff3dac32334040c07f0907
Kh-Ol-An <olegkhristenko@gmail.com>
2019-09-09 20:13:32 +0300

    Updates

---
+++ precache-manifest.39ab5c0c828692345579d753151af59f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cdaa8bee0b8f1b72ea091b9492184f",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "a7ab9a59399a7e1f398b",
+    "url": "/goit-react-hw-04-reader/static/css/main.53be36be.chunk.css"
+  },
+  {
+    "revision": "e42528cb974411211e7a",
+    "url": "/goit-react-hw-04-reader/static/js/2.ae8d0fc9.chunk.js"
+  },
+  {
+    "revision": "a7ab9a59399a7e1f398b",
+    "url": "/goit-react-hw-04-reader/static/js/main.9056173f.chunk.js"
+  },
+  {
+    "revision": "899a558441e20721c72b",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.39f47c04.js"
+  }
+]);
